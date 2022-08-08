d3553d0f089b42822887cfe9305ee099407fab95
see1rg <see1rg@mail.ru>
2022-08-08 22:38:53 +0300

    Сделал ДЗ

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,31 @@
 public class Homework {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+int i = 1;
+while (i < 10) {
+    System.out.print(i + " ");
+    i++;}
+System.out.println(i);
+for ( ; i > 0; i--)
+        {System.out.print( i + " ");}
+}
+public static void task2(){
+        int daysInMonth = 31;
+        for (int firstFriday = 3; firstFriday <= daysInMonth; firstFriday += 7  )
+        {System.out.println( "Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");}
+    }
 
+public static void task3(){
+        int periodCometa = 79;
+        int thisYear = 2022;
+        int beforeYear = thisYear - 200;
+        int afterYear = thisYear + 100;
+        for (int i = 0; i < afterYear ; i += periodCometa)
+            if ( i > beforeYear) {
+                System.out.println(i);}
     }
 }
